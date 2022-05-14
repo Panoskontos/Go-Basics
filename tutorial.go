2b0c7da207a0aa12d9f8eaa3883b9b2a11cba39b
Panagiotis Kontos <65974766+Panoskontos@users.noreply.github.com>
2022-05-14 13:52:39 +0300

    first

--- tutorial.go
+++ tutorial.go
@@ -6,4 +6,19 @@ import  "fmt"
 func main() {
 	// print line
 	fmt.Println("Welcome to my game")
+
+	var name string = "Panos"
+	// name = 23 will result in error
+	var id int = 4
+	var id2 uint = 4
+	var id3 float64 = 4.23
+	var id4 bool = true
+
+
+
+	fmt.Println(name)
+	fmt.Println(id)
+	fmt.Println(id2)
+	fmt.Println(id3)
+	fmt.Println(id4)
 }
