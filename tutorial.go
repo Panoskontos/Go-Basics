52a19251e2c3920ea399dd9b9785da5873c12306
Panagiotis Kontos <65974766+Panoskontos@users.noreply.github.com>
2022-05-14 13:56:39 +0300

    first

--- tutorial.go
+++ tutorial.go
@@ -7,6 +7,7 @@ func main() {
 	// print line
 	fmt.Println("Welcome to my game")
 
+	// Variables and data types
 	var name string = "Panos"
 	// name = 23 will result in error
 	var id int = 4
@@ -14,9 +15,12 @@ func main() {
 	var id3 float64 = 4.23
 	var id4 bool = true
 
+	// if you are bored and don't want to define type
+	name2 := "Giorgos"
 
 
 	fmt.Println(name)
+	fmt.Println(name2)
 	fmt.Println(id)
 	fmt.Println(id2)
 	fmt.Println(id3)
